57bfca6ae40793bb8eaa0d9c1350000e1a977295
torbob <nicholaswilson7803@gmail.com>
2026-03-19 14:48:25 +0700

    test

---
+++ main.py
@@ -0,0 +1,38 @@
+import os
+import sys
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from fastapi import FastAPI
+
+from model_flow.entity import Entity
+from model_flow.models import ner, qa, sentiment
+
+app = FastAPI()
+
+
+def predict_text_sentiment(text: str):
+    result = sentiment.get_text_sentiment(text)
+    return {
+        "status": "No entities were found, using text sentiment analysis",
+        "text": text,
+        "result": result,
+    }
+
+
+@app.get("/predict-entity")
+async def predict_entity_sentiment(text: str):
+    entities = ner.get_entity(text)
+
+    if len(entities) == 0:
+        return predict_text_sentiment(text)
+
+    qa.get_description(entities, text)
+    sentiment.get_entity_sentiment(entities)
+    return {"status": "Entities were found", "entities": entities}
+
+
+@app.get("/predict-text")
+async def predict_text_sentiment(text: str):
+    s = sentiment.get_text_sentiment(sentiment.clean_text(text))
+    return {"status": "Text sentiment analysis", "text": text, "result": s}
